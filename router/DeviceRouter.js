d4907ced54cfc25be46de642ddd03ed7f4cf2ce7
DharamSavsani <98221609+DharamSavsani@users.noreply.github.com>
2023-07-15 16:17:44 +0530

    Added getStatus api in deviceRouter

--- router/DeviceRouter.js
+++ router/DeviceRouter.js
@@ -8,11 +8,25 @@ const UserModel = require("../model/UserModel");
 DeviceRouter.post("/addDevice", async (req, res) => {
   const { _id, room, val } = req.body;
   try {
-    const user = await UserModel.findByIdAndUpdate({ _id }, { $set: {[room] : val} });
+    const user = await UserModel.findByIdAndUpdate(
+      { _id },
+      { $set: { [room]: val } }
+    );
     res.send(user);
   } catch (error) {
-    res.send(error)
+    res.send(error);
   }
 });
 
+// User can retrive data of Devices and also use for other varification
+DeviceRouter.get("/getStatus", async (req, res) => {
+  const _id = req.body._id;
+  const user = await UserModel.find({ _id })
+    .then((v) => {
+      res.send(v);
+    })
+    .catch((e) => {
+      res.send(e);
+    });
+});
 module.exports = DeviceRouter;
