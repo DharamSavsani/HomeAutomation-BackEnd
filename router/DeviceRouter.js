7c85941ea520fd90ee31ccf3a848454baef6e9a2
DharamSavsani <98221609+DharamSavsani@users.noreply.github.com>
2023-07-18 17:29:26 +0530

    lean() and exec()

--- router/DeviceRouter.js
+++ router/DeviceRouter.js
@@ -4,7 +4,7 @@ const express = require("express");
 const DeviceRouter = express.Router();
 const UserModel = require("../model/UserModel");
 
-//User can add Device using this API
+//User can add, delete, and set the Device using this API
 DeviceRouter.post("/addDevice", async (req, res) => {
   const { _id, room, val } = req.body;
   try {
@@ -20,13 +20,13 @@ DeviceRouter.post("/addDevice", async (req, res) => {
 
 // User can retrive data of Devices and also use for other varification
 DeviceRouter.post("/getStatus", async (req, res) => {
-  const _id = req.body._id;
-  const user = await UserModel.find({ _id })
-    .then((v) => {
-      res.send(v);
-    })
-    .catch((e) => {
-      res.send(e);
-    });
+  try {
+    const _id = req.body._id;
+    const user = await UserModel.find({ _id }).lean().exec();
+    res.send(user);
+  } catch (e) {
+    console.log("Error in getting device status", e);
+    res.send(e)
+  }
 });
 module.exports = DeviceRouter;
